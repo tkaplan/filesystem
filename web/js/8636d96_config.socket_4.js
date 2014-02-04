9977648db25c4815b5811718bca2c4c73d3e7a82
root <root@dev-VirtualBox.(none)>
2014-02-03 22:02:28 -0800

    Changed correct socket url.

--- web/js/8636d96_config.socket_4.js
+++ web/js/8636d96_config.socket_4.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://filesystem.tks-universe.com:3000');
 
 socket.on('tree update', function (data) {
 	console.log(data);
